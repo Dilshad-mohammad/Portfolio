44055eab8a2130d6a7effaea0707aee995869bc5
Dilshad Alam <Dilshadalam014@gmail.com>
2026-05-19 11:24:00 +0530

    resume updated

--- lib/view/intro/components/download_button.dart
+++ lib/view/intro/components/download_button.dart
@@ -10,7 +10,7 @@ class DownloadButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        launchUrl(Uri.parse('https://drive.google.com/file/d/1l-xcrk7sk-rRmHniMC_Iy6_WRiYwi_c-/view?usp=drive_link'));
+        launchUrl(Uri.parse('https://drive.google.com/file/d/1ISgs9ugMkhxUbe-50eJ21UNGgDSTgymE/view?usp=drive_link'));
         },
       child: Container(
         alignment: Alignment.center,
